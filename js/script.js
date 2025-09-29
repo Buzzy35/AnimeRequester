45d72c1b52b9699795bcddbd5bcbeb859d3a5ea9
Mathieu Degroux <degroux241@unicaen.fr>
2025-09-29 17:39:57 +0200

    ajoute de la fonction CreeCarte

--- js/script.js
+++ js/script.js
@@ -26,43 +26,69 @@ function getKey() {
 const KEY = getKey();
 console.log(KEY);
 
-const URL = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal`;
-
-
-
-async function getData() {
+async function getByName(name) {
     try {
-        const response = await fetch(URL, {
+        const response = await fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${name}`, {
             method: "GET",
             headers: {
                 'x-rapidapi-key': KEY,
                 'x-rapidapi-host': 'anime-db.p.rapidapi.com'
             }
         });
-
         if (!response.ok) {
             throw new Error("Erreur HTTP : " + response.status);
         }
 
         const data = await response.json();
-        console.log("Données reçues :", data);
+        CreeCartes(data.data); 
 
     } catch (error) {
         console.error("Erreur lors du fetch :", error);
     }
 }
 
-function getByName(name) {
+async function getByClassement(rank) {
+    try {
+        const response = await fetch(`https://anime-db.p.rapidapi.com/anime/by-ranking/${rank}`, {
+            method: "GET",
+            headers: {
+                'x-rapidapi-key': KEY,
+                'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur HTTP : " + response.status);
+        }
 
+        const data = await response.json();
+        CreeCartes(data.data); 
+
+    } catch (error) {
+        console.error("Erreur lors du fetch :", error);
+    }
 }
 
-function getByClassement() {
+async function getByID(id) {
+    try {
+        const response = await fetch(`https://anime-db.p.rapidapi.com/anime/by-id/${id}`, {
+            method: "GET",
+            headers: {
+                'x-rapidapi-key': KEY,
+                'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+            }
+        });
+        if (!response.ok) {
+            throw new Error("Erreur HTTP : " + response.status);
+        }
 
+        const data = await response.json();
+        CreeCartes(data.data); 
+
+    } catch (error) {
+        console.error("Erreur lors du fetch :", error);
+    }
 }
 
-function getByID(id) {
-
-}
 
 function onRecherche(e) {
     e.preventDefault();
@@ -84,6 +110,33 @@ function onRecherche(e) {
             break;
     }
 }
+
+function CreeCartes(anime) {
+    const container = document.getElementById("resultats");
+    
+    anime.forEach(anime => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <div>
+                <img src="${anime.image}" alt="${anime.title}">
+                <div>
+                    <h5>${anime.title}</h5>
+                    <p>
+                        <a>Genres:</a> ${anime.genres}<br>
+                        <a>Classement:</a> ${anime.ranking}<br>
+                        <a>Episodes:</a> ${anime.episodes}
+                    </p>
+                </div>
+                <div>
+                    <a>ID: ${anime._id}</a>
+                </div>
+            </div>
+        `;
+        container.appendChild(card);
+    });
+}
+
+
 
 
 
