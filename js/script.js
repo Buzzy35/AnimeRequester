1cd7e6175c13e984e68c1b02d8188851d0415772
Buzzy35 <imbuzzy35@gmail.com>
2025-10-08 16:35:45 +0200

    add categories

--- js/script.js
+++ js/script.js
@@ -171,6 +171,7 @@ function addAnime(anime) {
                 <h5 class="card-title">${anime.title}</h5>
                 <p class="card-text">Id : ${anime._id}</p>
                 <p class="card-text">Classement : ${anime.ranking}</p>
+                <p class="card-text">Categories : ${anime.genres}</p>
                 <p class="card-text">${anime.synopsis.slice(0, 100)}...</p>
                 <a href="${anime.link}" target="_blank" class="btn btn-primary">Voir sur MAL</a>
             </div>
